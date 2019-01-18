50d6bb8e1ff6428d68f277cf209a0bfa3b3a8a94
mocxd <mark.adkins.hastings@gmail.com>
2019-01-18 15:10:40 -0500

    edits list

--- source/_plugins/data_filters.rb
+++ source/_plugins/data_filters.rb
@@ -3,10 +3,10 @@ module Jekyll
 
     def find_data_reference_by_id(dataset, id, referenceField)
       data = []
-      dataset.map do |data_target| 
+      dataset.map do |data_target|
         unless data_target[referenceField].nil?
           data_target[referenceField].select do |ref|
-            if ref['sys']['id'] == id 
+            if ref['sys']['id'] == id
               data << data_target
             end
           end
@@ -17,7 +17,10 @@ module Jekyll
 
     def find_upcoming_data(dataset, dateField)
       data = []
-      dataset.map do | data_target | 
+      dataset.map do | data_target |
+        if !data_target['selfPaced'].nil? && data_target['selfPaced']
+          data << data_target
+        end
         unless data_target[dateField].nil?
           if data_target[dateField] > Time.now
             data << data_target
@@ -29,7 +32,7 @@ module Jekyll
 
     def find_past_data(dataset, dateField)
       data = []
-      dataset.map do | data_target | 
+      dataset.map do | data_target |
         unless data_target[dateField].nil?
           if data_target[dateField] < Time.now
             data << data_target
